889471fcf0fe901202faebead3e767812c8ba43a
Gleb Khaykin <khaykingleb@gmail.com>
2022-08-17 15:35:44 +0300

    feat: add s3 logging setup

--- terraform/modules/s3/main.tf
+++ terraform/modules/s3/main.tf
@@ -8,3 +8,21 @@ resource "aws_s3_bucket_acl" "this" {
   bucket = aws_s3_bucket.this.id
   acl    = "private"
 }
+
+resource "aws_s3_bucket" "log_bucket" {
+  bucket = "${var.tags["project_name"]}-log-bucket"
+
+  tags = merge({ name = "OCP-S3-Log-Bucket" }, var.tags)
+}
+
+resource "aws_s3_bucket_acl" "log_bucket_acl" {
+  bucket = aws_s3_bucket.log_bucket.id
+  acl    = "log-delivery-write"
+}
+
+resource "aws_s3_bucket_logging" "example" {
+  bucket = aws_s3_bucket.this.id
+
+  target_bucket = aws_s3_bucket.log_bucket.id
+  target_prefix = "log/"
+}
