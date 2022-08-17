36bab375436ab1760197a990603cd0380c2301e9
Gleb Khaykin <khaykingleb@gmail.com>
2022-08-17 20:35:37 +0300

    feat: add iam roles & policies

---
+++ terraform/modules/security/outputs.tf
@@ -0,0 +1,16 @@
+/*
+output "iam_erm_service_role_arn" {
+  description = "The Amazon Resource Name specifying the ERM service role."
+  value       = aws_iam_role.emr_service.arn
+}
+
+output "iam_instance_profile_arn" {
+  description = "The Amazon Resource Name assigned by AWS to the instance profile."
+  value       = aws_iam_instance_profile.this.arn
+}
+
+output "ssh_key_name" {
+  description = "The key pair name used to control login access to EC2 instances."
+  value       = aws_key_pair.this.key_name
+}
+*/
